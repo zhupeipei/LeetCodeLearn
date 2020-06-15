545145d69e72deb4ecc286ec7283245460ae3fde
zhupeipei <zhupeipei91@163.com>
2020-06-15 23:59:26 +0800

    最长不重复子序列

---
+++ java/src/com/aire/Exercise003.java
@@ -0,0 +1,44 @@
+package com.aire;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Exercise003 {
+    public static void main(String[] args) {
+        System.out.println(new Solution().lengthOfLongestSubstring("dvdf"));
+    }
+
+    static class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            if (s == null || s.length() == 0) {
+                return 0;
+            }
+            int n = s.length();
+            int[] dp = new int[n];
+            dp[0] = 1;
+            for (int i = 1; i < n; i++) {
+                char ch = s.charAt(i);
+                int j = i - 1;
+                boolean flag = false;
+                for (; j >= i - dp[i - 1]; j--) {
+                    char c = s.charAt(j);
+                    if (c == ch) {
+                        flag = true;
+                        dp[i] = i - j;
+                        break;
+                    }
+                }
+                if (!flag) {
+                    dp[i] = dp[i - 1] + 1;
+                }
+            }
+            int max = 1;
+            for (int i = 0; i < n; i++) {
+                if (max < dp[i]) {
+                    max = dp[i];
+                }
+            }
+            return max;
+        }
+    }
+}
