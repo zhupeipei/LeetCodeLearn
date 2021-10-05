6be3d6a8f5ef9b296173229ee0ab2a7b48ef30ed
zhupeipei <peipei.zhu@ximalaya.com>
2021-10-05 16:36:24 +0800

    [feat] 动态代理

---
+++ src/main/java/com/aire/proxy/User.java
@@ -0,0 +1,22 @@
+package com.aire.proxy;
+
+/**
+ * Created on 2021/10/2 3:18 下午.
+ *
+ * @Author ZhuPeipei
+ */
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
