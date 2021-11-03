ec74f4144e6750619a883afb4ff6f9f2320fd3ce
zhupeipei <zhupeipei91@163.com>
2021-11-04 02:20:44 +0800

    [feat] 295. 数据流的中位数

---
+++ src/main/java/com/aire/Exercise295.java
@@ -0,0 +1,33 @@
+package com.aire;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * Created on 2021/11/4 上午2:14.
+ *
+ * @Author ZhuPeipei
+ */
+public class Exercise295 {
+    public static void main(String[] args) {
+
+    }
+
+    private List<Integer> bigList = new ArrayList<>(); // 这里的
+    private List<Integer> smallList = new ArrayList<>();
+
+    // 295. 数据流的中位数
+    public Exercise295() {
+        PriorityQueue
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+
+    }
+}
